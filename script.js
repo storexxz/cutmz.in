862f2b757bece6644ef9c871949ceb51d0075fed
adrinathti123-dot <adrinathti123@gmail.com>
2025-11-21 14:51:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionID) {
+    const sections = document.querySelectorAll(".section");
+
+    sections.forEach(sec => {
+        sec.classList.remove("active");
+    });
+
+    document.getElementById(sectionID).classList.add("active");
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
