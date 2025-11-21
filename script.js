1b8e983f240d0aeb9542dbfa54d74837806d8bf0
store.in <adrinathti123@gmail.com>
2025-11-21 19:13:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,3 @@ function showSection(sectionID) {
     document.getElementById(sectionID).classList.add("active");
     window.scrollTo({ top: 0, behavior: "smooth" });
 }
-
-// Falling leaves
-for(let i=0; i<15; i++){
-    const leaf = document.createElement('div');
-    leaf.classList.add('leaf');
-    leaf.style.left = Math.random() * window.innerWidth + 'px';
-    leaf.style.animationDuration = (5 + Math.random() * 5) + 's';
-    leaf.style.animationDelay = Math.random() * 5 + 's';
-    document.body.appendChild(leaf);
-}
