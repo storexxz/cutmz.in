e95f87d7df473c53c22e1cd176de725c7eab3f7b
store.in <adrinathti123@gmail.com>
2025-11-21 20:22:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDonate() {
+    document.getElementById("donate-popup").style.display = "block";
+}
+
+function closeDonate() {
+    document.getElementById("donate-popup").style.display = "none";
+}
+
