9ddd6bb2e195a2f6e15ed3f58e13c4aa429c4d53
store.in <adrinathti123@gmail.com>
2025-11-21 19:41:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Section switching
-function showSection(sectionID) {
-    const sections = document.querySelectorAll(".section");
-    sections.forEach(sec => sec.classList.remove("active"));
-    document.getElementById(sectionID).classList.add("active");
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
